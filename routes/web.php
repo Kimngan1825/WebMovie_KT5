cb6a63318d21f4ce5df88b2aa90006279bd10b2a
NHGiaHan <ngohaigiahan@gmail.com>
2026-04-08 14:08:22 +0700

    Câu 4: thêm phim mới và kiểm tra dữ liệu

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OpenRouterController;
+use App\Http\Controllers\MovieController4;
 
-Route::get('/', [App\Http\Controllers\MovieController::class, 'index']);
-
-
+Route::get('/', [App\Http\Controllers\MovieController4::class, 'index']);
 
 Route::get('/openrouter', [OpenRouterController::class, 'chat']);
+
+
+Route::get('/movies/create', [MovieController4::class, 'create'])->name('movies.create');
+Route::post('/movies/store', [MovieController4::class, 'store'])->name('movies.store');
